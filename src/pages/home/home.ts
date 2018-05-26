6a9335ced6ec6f8b37c8dc4b34339155dfca555a
LAPTOP-CDGAOKBU\Asus <putu.yasa2@gmail.com>
2018-05-27 00:22:09 +0700

    modif header dan tabs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
+    let loading = this.loadingCtrl.create({
+      spinner: 'hide',
+      content: ''
+    });
+  
+    loading.present();
+  
 
+    setTimeout(() => {
+      loading.dismiss();
+    }, 5000);
+    
   }
 
 }
